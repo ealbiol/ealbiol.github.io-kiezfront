824b64dac61fe886238365cd878c37833eb11a0b
ealbiol <eduard.albiol@gmail.com>
2021-07-12 12:50:08 +0200

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "https://kiezberlinback.herokuapp.com/:19903"
+export const BASE_URL = "https://kiezberlinback.herokuapp.com"
+
+
+//export const BASE_URL = "https://kiezberlinback.herokuapp.com/:19903"
