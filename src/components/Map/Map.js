b2024280f8553cc77ed06e713daa6b620ee12118
ealbiol <eduard.albiol@gmail.com>
2021-07-21 01:18:33 +0200

    Adding Descriptions

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -255,35 +255,14 @@ export default function Map({ NeighborhoodsDynamic }) {
                 key={index}
                 position={{ lat: neighborhood.lat, lng: neighborhood.lng }}
                 name={neighborhood.name}
-                onClick={() => irABarrio(neighborhood.name)}                //Dentro de un .map podemos llamar directamente a una propiedad del objeto en vez de (e).
+                onClick={() => irABarrio(neighborhood.name)}
                 label={neighborhood.name.toUpperCase()}
                 scaledSize={new window.google.maps.Size(90, 90)}
                 className="marker"
-              // icon={
-              //   {
-              //     url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-              //     scaledSize: new window.google.maps.Size(90, 90)
-              //   }
-              // }
-
-              // icon={{ url: props.iconUrl, scaledSize: props.iconSize }}
-
-              // icon={{
-              //   path:
-              //     'M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48v-41.6c0-74.2-60.2-134.4-134.4-134.4z',
-              //   fillColor: '#014175',
-              //   fillOpacity: 1,
-              //   scale: 0.075,
-              //   strokeColor: '#014175',
-              //   scaledSize: new window.google.maps.Size(25, 25),
-              // }}
-
               />
             )
           })
         }
-
-
       </GoogleMap>
     </div>
   ) : <></>
--- src/components/TechDescription/TechDescription.js
+++ src/components/TechDescription/TechDescription.js
@@ -4,7 +4,13 @@ import mixins from "../../images/mixins.jpg"
 import variables from "../../images/variables.jpg"
 import ampersands from "../../images/ampersands.jpg"
 import carousel from "../../images/carousel.gif"
-
+import find from "../../images/find.jpg"
+import list from "../../images/list.jpg"
+import map from "../../images/map.jpg"
+import greeting from "../../images/greeting.jpg"
+import math from "../../images/math.jpg"
+import mapComponent from "../../images/mapComponent.jpg"
+import filter from "../../images/filter.jpg"
 
 
 
@@ -67,26 +73,143 @@ export default function TechDescription() {
             {/* CAROUSEL */}
             <div className="TechDescription-in-Technologies__heroku-description-main" >
                 <div className="Heroku-left" >DYNAMIC CAROUSEL
-                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >Sass is used with the objective to shorten the style code:
-                        <div>1. The Nesting Ampersand.</div>
-                        <div>2. Variables.</div>
-                        <div>3. Mixins to shorten code.</div>
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >One of the first things we see when logging in is the Carousel of neighborhoods.
+                        <div>We get the Neighborhood names from its API. Those names are showing up randomly each time as the Math.random() method is applied.
+                        </div>
+                        <div>An onClick function is created to capture the name of the neighborhood and therefore redirect you to its profile page with the method .push with useHistory hook.
+                        </div>
                     </div>
                 </div>
 
+                <div  >
+                    <div  >
+                        <div>Carousel</div>
+                        <img src={carousel} width={"800px"} alt="ampersands" />
+                    </div>
+                    <div>
+                        <img src={math} width={"1100px"} alt="ampersands" />
+                    </div>
+                </div>
+            </div>
+
+
+            {/* NPM REACT-YOUTUBE-BACKGROUND */}
+            <div className="TechDescription-in-Technologies__heroku-description-main" >
+                <div className="Heroku-left" >react-youtube-background
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >Secondly we also see the background video.
+                        <div>1. It's been created downloading the component React-Youtube-Background from the website NPM.
+                        </div>
+                        <div>2. We use the prop videoId=youtubeID and the useState for the unique code of each youtube video.</div>
+                    </div>
+                </div>
 
                 <div>
-                    <div>Carousel</div>
-                    <img src={carousel} width={"400px"} alt="ampersands" />
+                    <div>NPM Component</div>
+                    <img src={carousel} width={"1000px"} alt="ampersands" />
                 </div>
+
+            </div>
+
+
+
+            {/* LOOK FOR A KIEZ COMPONENT */}
+            <div className="TechDescription-in-Technologies__heroku-description-main" >
+                <div className="Heroku-left" >List Component
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >
+                        <div>Scrolling-down we can find the full list of neighborhoods which renders depending on its boolean button state.
+                        </div>
+                        <div>A ".filter" is applied with the name of the variable NeighborhoodsDynamic which will give us all the areas matching what's been written in the input.</div>
+                    </div>
+                </div>
+
                 <div>
-                    <div>Math.random()</div>
-                    <img src={variables} width={"400px"} alt="variables" />
+                    <div>Neighborhoods</div>
+                    <img src={list} width={"1000px"} alt="ampersands" />
                 </div>
+
+            </div>
+
+
+
+            {/* GOOGLE MAPS COMPONENT */}
+            <div className="TechDescription-in-Technologies__heroku-description-main" >
+                <div className="Heroku-left" >Google Maps Component
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >Google maps implented downloading the "react-google-maps" component from the NPM site.
+                        <div>1. First we create its own component.
+                        </div>
+                        <div>2. We use the same filtering state for both the neighborhoods list and the map pin's so that both react at the same time.
+                        </div>
+                        <div>3. We add an onClick function to the pins that redirects you to the clicked area.
+                        </div>
+                        <div>4. When it comes to the map styles I used a created one from https://snazzymaps.com/ .</div>
+                    </div>
+                </div>
+
                 <div>
-                    <div>history.push</div>
-                    <img src={mixins} width={"400px"} alt="mixins" />
+                    <img src={mapComponent} width={"575px"} alt="ampersands" />
+                    <img src={map} width={"575px"} alt="ampersands" />
                 </div>
+
+            </div>
+
+
+
+
+            {/* USER GREETING */}
+            <div className="TechDescription-in-Technologies__heroku-description-main" >
+                <div className="Heroku-left" >User Greeting
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >
+                        <div>1. We create a cookike with localstorage.
+                        </div>
+                        <div>2. We use the useEffect hook to obtain the cookie userName amendment in the localstorage.
+                        </div>
+
+                    </div>
+                </div>
+
+                <div>
+                    <div>Greeting</div>
+                    <img src={greeting} width={"700px"} alt="ampersands" />
+                </div>
+
+            </div>
+
+
+
+            {/* FIND YOUR KIEZ */}
+            <div className="TechDescription-in-Technologies__heroku-description-main" >
+                <div className="Heroku-left" >Find Your Kiez
+                    <div className="TechDescription-in-Technologies__heroku-description-main__heroku-explanation" >Page where the user obtains a neighborhood based on chosen criteria.
+
+                        <div>In this page the user uses a SLIDE imported from material-ui.
+                            On it the neighborhood properties are chosen. When applied on the right side we will see the number of neighborhoods per district that match with the desired criteria.</div>
+                        <div>It has been built in the following way:
+                        </div>
+                        <div>On the CLIENT side:
+                        </div>
+                        <div>
+                            1. We create a State that contains ALL the the property values initially EMPTY.
+                        </div>
+                        <div>2. When a Slide is moved to a specific property value it will amend the value in our object from EMPTY to the chosen value.
+                        </div>
+                        <div>3. Example: if Architecture Predominance is moved to "Historic", the state will amend our object Architecture Predominance value from EMPTY to HISTORIC.</div>
+                        <div>On the <b> SERVER SIDE</b>:
+                        </div>
+                        <div>1. It recibes the data from the <b> CLIENT </b>(the state) which contains all the values obtained through the Slides.
+                        </div>
+                        <div>2. A .find is applied having into account all the values. If the value is empty it won't count it.
+                        </div>
+                        <div>3. Finally all neighborhoods that match with the chosen property values are given back to the front.
+                        </div>
+                    </div>
+                </div>
+
+                <div>
+                    <div>Page</div>
+                    <img src={find} width={"500px"} alt="ampersands" />
+                    <img src={filter} width={"500px"} alt="ampersands" />
+                </div>
+
             </div>
 
 
